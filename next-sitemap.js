6e4a5a6880e7a927bafdf6e89c483805bd0a0ee6
JD Fillmore <jd@infused.agency>
2022-04-03 16:42:04 -0400

    add additional sitemap

--- next-sitemap.js
+++ next-sitemap.js
@@ -4,4 +4,5 @@ module.exports = {
   siteUrl: "https://www.premiumdeliverys.com/",
   generateRobotsTxt: true,
   outDir: "./out",
+  additionalSitemaps: ["https://www.premiumdeliverys.com/sitemap-0.xml"],
 };
